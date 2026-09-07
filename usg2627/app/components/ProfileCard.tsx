18b951e005d3cfbe4951975ffbb5883885afefe6
Ian Glenn Curilan <iancurilan1027@gmail.com>
2026-09-07 19:19:06 +0800

    fix: adjustment

--- usg2627/app/components/ProfileCard.tsx
+++ usg2627/app/components/ProfileCard.tsx
@@ -214,11 +214,11 @@ export default function ProfileCard({
                 href={facebookUrl}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="flex h-8 w-8 items-center justify-center rounded-full bg-[#173490] text-white hover:bg-[#1e4bb8] shadow-xs transition"
+                className="flex h-8 w-8 items-center justify-center rounded-full bg-[#1877F2] text-white hover:bg-[#166fe5] shadow-xs transition"
                 aria-label="Facebook Profile"
               >
-                <svg className="h-4 w-4 fill-currentColor" viewBox="0 0 24 24">
-                  <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
+                <svg className="h-4.5 w-4.5 fill-white" viewBox="0 0 24 24">
+                  <path d="M9.101 23.691v-7.98H6.627v-3.667h2.474v-1.58c0-4.085 1.848-5.978 5.858-5.978.401 0 .955.042 1.468.103a8.68 8.68 0 0 1 1.77.386v3.315c-.473-.048-1.026-.073-1.637-.073-1.897 0-2.482.723-2.482 2.378v1.449h4.241l-.592 3.667h-3.649v7.98H9.101z" />
                 </svg>
               </a>
             </div>
